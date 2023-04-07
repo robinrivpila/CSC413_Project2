3e650cce7af131a636b40996ec85c72e7d0e6e02
Robin Pilapil <robin.pilapil@gmail.com>
2023-04-06 21:24:05 -0700

    checked to see if it worked

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        //new InventoryGUI();
+        new InventoryGUI();
 
 
         Container container1 = new Container(1, 12, "Me" , "you", "Banana");
