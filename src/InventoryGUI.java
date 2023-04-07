78e754d1f6a082416cc69d2b54ee566987f37073
Robin Pilapil <robin.pilapil@gmail.com>
2023-04-06 21:21:03 -0700

    added to GUI

--- src/InventoryGUI.java
+++ src/InventoryGUI.java
@@ -13,7 +13,28 @@ public class InventoryGUI extends JFrame implements ActionListener {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         pane = new JPanel();
-       // GridLayout
+        GridLayout layout = new GridLayout(6,2);
+        pane.setLayout(layout);
+
+        JLabel idLabel = new JLabel("ID Number: ");
+        pane.add(idLabel);
+
+        JLabel weightLabel = new JLabel("Weight(kg): ");
+        pane.add(weightLabel);
+
+        JLabel senderLabel = new JLabel("Sender: ");
+        pane.add(senderLabel);
+
+        JLabel receiverLabel = new JLabel("Receiver: ");
+        pane.add(receiverLabel);
+
+        JLabel descriptionLabel = new JLabel("Description: ");
+        pane.add(descriptionLabel);
+
+        setContentPane(pane);
+        pack();
+        setVisible(true);
+
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
+        //new InventoryGUI();
+
 
         Container container1 = new Container(1, 12, "Me" , "you", "Banana");
         Container container2 = new Container(1, 10, "You", "Me", "notebooks");
@@ -8,5 +11,7 @@ public class Main {
         inventory1.addContainer(container1);
         inventory1.addContainer(container2);
         inventory1.print();
+
+
     }
 }
